6a32bb95e5e9c8dadc28c8a45e45c71e1b7a7060
hsnlbnan <hsnlbnan@gmail.com>
2023-03-05 19:33:31 +0300

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,24 @@
 <svelte:head>
   <title>Welcome to Husnu's Website</title>
   <meta name="description" content="Svelte demo app" />
+
+  <meta property="og:title" content="Welcome to Husnu's Website" />
+  <meta
+    property="og:description"
+    content="Husnu Lubnan is a Frontend Developer who loves to build things with JavaScript. He is currently working at a @Nono Company"
+  />
+  <meta property="og:image" content="/og.png" />
+  <meta property="og:url" content="https://husnu.dev" />
+
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:site" content="@hsnlbnan" />
+  <meta name="twitter:creator" content="@hsnlbnan" />
+  <meta name="twitter:title" content="Welcome to Husnu's Website" />
+  <meta
+    name="twitter:description"
+    content="Husnu Lubnan is a Frontend Developer who loves to build things with JavaScript. He is currently working at a @Nono Company"
+  />
+  <meta name="twitter:image" content="/og.png" />
 </svelte:head>
 
 <section class="wallpaper" aria-label="Welcome to SvelteKit">
